e2b5388ceaf8c424271272f729ec4d3f3a76c3bc
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-05-10 10:25:09 -0700

    add "/" para consumir AuthAPI

--- Pessoas.Domain/Interfaces/APIs/IAuthApi.cs
+++ Pessoas.Domain/Interfaces/APIs/IAuthApi.cs
@@ -7,6 +7,6 @@ namespace Aluno.Core.Domain.Interfaces.APIs;
 
 public interface IAuthApi
 {
-    [Post("v1/usuario")] Task<BaseResponse<UsuarioViewModel>> RegistrarAsync(UsuarioNovoViewModel user);
-    [Put("v1/usuario")] Task<BaseResponse<object>> AtualizarAsync(UsuarioAtualizarViewModel user);
+    [Post("/v1/usuario")] Task<BaseResponse<UsuarioViewModel>> RegistrarAsync(UsuarioNovoViewModel user);
+    [Put("/v1/usuario")] Task<BaseResponse<object>> AtualizarAsync(UsuarioAtualizarViewModel user);
 }
