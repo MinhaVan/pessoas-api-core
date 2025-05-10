72d1e4eaeeeb95055805dafbd0a12d51f07261d4
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-05-09 18:29:21 -0700

    ajustando Program.cs

--- Pessoas.API/Program.cs
+++ Pessoas.API/Program.cs
@@ -3,72 +3,90 @@ using System.IO;
 using AspNetCoreRateLimit;
 using Aluno.Core.API.Extensions;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Aluno.Core.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-var environment = builder.Environment.EnvironmentName;
-Console.WriteLine($"Iniciando a API no ambiente '{environment}'");
-
-builder.Configuration
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables();
-
-// Adiciona as configurações do Secrets Manager
-var secretManager = builder.Services.AddSecretManager(builder.Configuration);
-
-// Configura os serviços
-builder.Services.AddCustomAuthentication(secretManager)
-                .AddCustomAuthorization()
-                .AddCustomDbContext(secretManager)
-                .AddCustomSwagger()
-                .AddCustomRateLimiting(secretManager)
-                .AddCustomResponseCompression()
-                .AddCustomCors()
-                .AddCustomServices(secretManager)
-                .AddCustomRepository(secretManager)
-                .AddCustomMapper()
-                .AddControllersWithFilters()
-                .AddCustomHttp(secretManager);
-
-// Configura o logger
-builder.Logging.ClearProviders().AddConsole().AddDebug();
-
-var app = builder.Build();
-
-// Configurações específicas para desenvolvimento
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseDeveloperExceptionPage();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var environment = builder.Environment.EnvironmentName;
+        Console.WriteLine($"Iniciando a API no ambiente '{environment}'");
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        // Adiciona as configurações do Secrets Manager
+        var secretManager = builder.Services.AddSecretManager(builder.Configuration);
+
+        // Configura os serviços
+        builder.Services.AddCustomAuthentication(secretManager)
+                        .AddCustomAuthorization()
+                        .AddCustomDbContext(secretManager)
+                        .AddCustomSwagger()
+                        .AddCustomRateLimiting(secretManager)
+                        .AddCustomResponseCompression()
+                        .AddCustomCors()
+                        .AddCustomServices(secretManager)
+                        .AddCustomRepository(secretManager)
+                        .AddCustomMapper()
+                        .AddControllersWithFilters()
+                        .AddCustomHttp(secretManager);
+
+        // Configura o loggerbuilder.Logging.AddConsole();
+        builder.Logging.AddConsole().AddDebug();
+
+        var app = builder.Build();
+
+        // Configurações específicas para desenvolvimento
+        if (environment == "local")
+        {
+            app.UseDeveloperExceptionPage();
+            app.UsePathBase("/pessoas");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/pessoas/swagger/v1/swagger.json", "Pessoas.API v1");
+                c.RoutePrefix = "swagger";
+            });
+        }
+        else
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                Console.WriteLine($"Rodando migrations '{environment}'");
+                var db = scope.ServiceProvider.GetRequiredService<APIContext>();
+                db.Database.Migrate();
+                Console.WriteLine($"Migrations '{environment}' executadas com sucesso");
+            }
+
+            app.UsePathBase("/auth");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pessoas.API v1");
+                c.RoutePrefix = "swagger";
+            });
+        }
+
+        app.UseResponseCompression();
+        app.UseRouting();
+        app.UseIpRateLimiting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseCors("CorsPolicy");
+        app.UseWebSockets();
+        app.MapControllers();
+
+        Console.WriteLine("Configuração de API finalizada com sucesso!");
+
+        app.Run();
+    }
 }
-
-// Configurações gerais
-#if !DEBUG
-app.UsePathBase("/pessoas");
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/pessoas/swagger/v1/swagger.json", "Pessoas.API v1");
-    c.RoutePrefix = "swagger";
-});
-#else
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pessoas.API v1"));
-#endif
-
-app.UseResponseCompression();
-app.UseRouting();
-app.UseIpRateLimiting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseCors("CorsPolicy");
-app.UseWebSockets();
-app.MapControllers();
-
-Console.WriteLine("Configuração de API finalizada com sucesso!");
-
-app.Run();
